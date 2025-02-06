522aa1296b138c8ecbf1ee69902e24706b7549fa
joshua.robbins <joshua.robbins@clario.com>
2025-02-06 09:35:27 -0500

    Upgrade MPS to 2023.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,33 @@
 plugins {
-    id("com.specificlanguages.mps") version "1.2.2"
+    id("com.specificlanguages.mps") version "1.8.0"
     `maven-publish`
 }
 
 repositories {
-    maven(url = "https://projects.itemis.de/nexus/content/repositories/mbeddr")
+    maven { url = uri("https://artifacts.itemis.cloud/repository/maven-mps/") }
     mavenCentral()
     // JitPack should come last since it will build things on demand
     maven(url = "https://jitpack.io")
+    mavenLocal() // TEMP: REMOVE ME
 }
 
 dependencies {
-    "mps"("com.jetbrains:mps:2021.1.3@zip")
-    "generation"("com.specificlanguages:mps-json:1.0.0@zip")
+    "mps"("com.jetbrains:mps:2023.2@zip")
+    "generation"("com.specificlanguages:mps-json:2.0.0@zip")
 }
 
 stubs {
     register("stubs") {
         destinationDir("solutions/com.specificlanguages.dependencies/lib")
 
-        // These versions are used by MPS 2021.1.3
+        // These versions are used by MPS 2023.2
         dependency("org.apache.commons:commons-lang3:3.10")
         dependency("com.fasterxml.jackson.core:jackson-databind:2.12.0")
     }
 }
 
 group = "com.specificlanguages"
-version = "0.0.7"
+version = "0.1.0"
 
 publishing {
     publications {
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://artifacts.itemis.cloud/repository/maven-mps/") }
+        gradlePluginPortal()
+    }
+}
